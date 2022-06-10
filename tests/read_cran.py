3f2f259c54642da26da0b8dde53377c38bdf1c85
Nadia <nadia.glez99@gmail.com>
2022-06-09 22:34:36 -0400

    docs are read

--- tests/read_cran.py
+++ tests/read_cran.py
@@ -4,25 +4,35 @@ import re
 def extract_info(path_docs: str, path_qry: str, path_rel: str):
     with open(path_docs) as doc_file:
         docs = doc_file.read()
-        print(type(docs))
         parse_docs(docs)
 
     with open(path_qry) as qry_file:
         queries = qry_file.read()
-        print(type(queries))
         parse_queries(queries)
+
+
+def parse_docs(text: str):
+    tokens = text.split()
+    docs = []
+    i = 0
+    while i < len(tokens):
+        if tokens[i] == '.W':
+            j = i + 1
+            current_doc = ""
+            while j < len(tokens) and not tokens[j] == '.I':
+                current_doc += tokens[j]
+                j += 1
+            docs.append(current_doc)
+            i = j + 1
+        i += 1
+    print(f'Docs read: {len(docs)}')
 
 
 # returns a list of queries in order
 def parse_queries(text: str):
     queries = re.split('.I \d+\n.W', text)[1:]
-
-
-def parse_docs(text: str):
-    docs = re.split('.I \d+\n.T\n.+?(?=.A)\n.B.+?(?=.W)')
-    # docs = re.split('.I\d+\n.T\w+\n.A\w+\n.B\w+\n.W', text)
-    # docs = re.findall('?=\.T', text)
-    print(docs)
+    print(f'Queries read: {len(queries)}')
+    print(queries[0])
 
 
 path_docs = "cran/cran.all.1400"
