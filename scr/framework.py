f9bc73e9cdb990b6d1f93a83da3756adda3c840b
Alejandro Labourdette <jal.labourdette@gmail.com>
2022-06-19 03:58:28 +0200

    Framework improve efficency

--- scr/framework.py
+++ scr/framework.py
@@ -7,10 +7,10 @@ from utils import DocumentCollection, Query
 class VectorFramework:
     def __init__(self, document_collection: DocumentCollection, load_document=True):
         self.alpha = 0.4
-        self.similarity_umbral = 0
+        self.similarity_umbral = 0.3
         if load_document:
             try:
-                with open('document_collection.pickle', 'rb') as infile:
+                with open('scr/document_collection.pickle', 'rb') as infile:
                     document_collection = pickle.load(infile)
             except FileNotFoundError:
                 self._process_and_save_dc(document_collection)
@@ -22,24 +22,14 @@ class VectorFramework:
     def _process_and_save_dc(self, document_collection):
         self.document_collection = document_collection
         self.compute_documents_weights()
-        with open('document_collection.pickle', 'wb') as outfile:
+        with open('scr/document_collection.pickle', 'wb') as outfile:
             pickle.dump(document_collection, outfile)
 
-    def _get_max_freq_in_document(self, document_id):
-        dc = self.document_collection
-        maximum = 0
-        for doc_id, term_id in dc.frequencies.keys():
-            if doc_id == document_id:
-                maximum = max(maximum, dc.frequencies[doc_id, term_id])
-        return maximum
 
     def _compute_tf(self, document_id, term_id):
         dc = self.document_collection
-        max_freq = self._get_max_freq_in_document(document_id)
-        try:
-            freq = dc.frequencies[(document_id, term_id)]
-        except KeyError:
-            freq = 0
+        max_freq = dc.max_frequency
+        freq = dc.frequencies[(document_id, term_id)]
         return freq/max_freq
 
     def _get_amount_of_doc_that_have_term(self, term_id):
@@ -67,25 +57,33 @@ class VectorFramework:
     def compute_documents_weights(self):
         dc = self.document_collection
 
+        i = 1
+        total = len(dc.t_id2name.keys())
         for term_id in dc.t_id2name.keys():
+            print('IDF: ', i, '/', total)
+            i += 1
             dc.idf[term_id] = self._compute_idf(term_id)
-        for doc_id in dc.d_id2name.keys():
-            for term_id in dc.t_id2name.keys():
-                weight = self._compute_document_weight(doc_id, term_id)
-                dc.weights_doc[doc_id, term_id] = weight
+
+        i = 1
+        total = len(dc.frequencies.keys())
+        for doc_id, term_id in dc.frequencies.keys():
+            print('Document Weight: ', i, '/', total)
+            i += 1
+            weight = self._compute_document_weight(doc_id, term_id)
+            dc.weights_doc[doc_id, term_id] = weight
 
     def compute_query_weights(self, query: Query):
         dc = self.document_collection
         max_freq = max(query.frequencies.values())
-        for term_name in dc.t_name2id.keys():
-            term_id = dc.t_name2id[term_name]
+        for term_name in query.frequencies.keys():
             try:
+                term_id = dc.t_name2id[term_name]
                 q_term_freq = query.frequencies[term_name]
                 a = self.alpha + (1 - self.alpha) * q_term_freq / max_freq
                 b = dc.idf[term_id]
                 query.weights[term_id] = a * b
-            except KeyError:  # Term it's not in query
-                query.weights[term_id] = 0
+            except KeyError: # term is not in vocabulary
+                continue
         return query
 
     def _sim_of_document(self, document_id, query: Query):
@@ -94,16 +92,22 @@ class VectorFramework:
         a = 0  # refers to numerator in similarity equation
         b = 0
         c = 0
-        for term_id in dc.t_id2name.keys():
-            q_weight = query.weights[term_id]
-            d_weight = dc.weights_doc[(document_id, term_id)]
+        for term_id in list(query.weights.keys()) + dc.terms_id_of_document_id[document_id]:
+            try:
+                q_weight = query.weights[term_id]
+            except KeyError:
+                q_weight = 0
+            try:
+                d_weight = dc.weights_doc[(document_id, term_id)]
+            except KeyError:
+                d_weight = 0
             a = a + q_weight * d_weight
             b = b + d_weight * d_weight
             c = c + q_weight * q_weight
         b = math.sqrt(b)
         c = math.sqrt(c)
 
-        if c == 0:
+        if b == 0 or c == 0: # document is empty, or query have no term of vocabulary
             return 0
         return a / (b * c)
 
@@ -120,7 +124,7 @@ class VectorFramework:
                     documents_similarity[str(similarity)] = [doc_id]
 
         similarities_selected = list(documents_similarity.keys())
-        similarities_selected.sort()
+        similarities_selected.sort(reverse= True)
         return_list_id = []
         for val in similarities_selected:
             return_list_id.extend(documents_similarity[val])
@@ -128,4 +132,4 @@ class VectorFramework:
         return_list_name = []
         for doc_id in return_list_id:
             return_list_name.append(self.document_collection.d_id2name[doc_id])
-        return return_list_name
+        return return_list_name, return_list_id
